5acb4ffebc0231296a409b12eb917d5bb4b2ced3
Exequiel Arroyo <zekielarroyo@gmail.com>
2022-04-28 19:54:14 +0800

    initial commit to crithm

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+    <div className='App'>
+      <header className='App-header'>
+        <img src={'/assets/logo.png'} className='App-logo' alt='logo' />
+        <div className='content'>
+          <p>Crithm is a small company.</p>
+          <p id='subtitle'>This website is on development.</p>
+        </div>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
