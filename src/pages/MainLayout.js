b927609dfb944b0b7e43565fe1a1119cf6fb8911
Exequiel Arroyo <zekielarroyo@gmail.com>
2022-04-29 21:18:50 +0800

    fix warning

--- src/pages/MainLayout.js
+++ src/pages/MainLayout.js
@@ -6,14 +6,14 @@ const MainLayout = () => {
     <>
       <nav className='navbar navbar-expand-lg navbar-dark'>
         <div className='container'>
-          <a className='navbar-brand' href='#'>
+          <a className='navbar-brand' href='/'>
             <img src='/assets/images/logo.png' alt='logo' height='24' />
           </a>
           <button
             className='navbar-toggler'
             type='button'
             data-bs-toggle='collapse'
-            data-bs-target='#navbarNav'
+            data-bs-target='/navbarNav'
             aria-controls='navbarNav'
             aria-expanded='false'
             aria-label='Toggle navigation'>
@@ -22,42 +22,42 @@ const MainLayout = () => {
           <div className='collapse navbar-collapse navv' id='navbarNav'>
             <ul className='navbar-nav'>
               <li className='nav-item'>
-                <a className='nav-link active' aria-current='page' href='#'>
+                <a className='nav-link active' aria-current='page' href='/'>
                   Home
                 </a>
               </li>
               <li className='nav-item'>
-                <a className='nav-link' href='#'>
+                <a className='nav-link' href='/'>
                   Creative Software
                 </a>
               </li>
               <li className='nav-item'>
-                <a className='nav-link' href='#'>
+                <a className='nav-link' href='/'>
                   Process
                 </a>
               </li>
               <li className='nav-item'>
-                <a className='nav-link ' href='#'>
+                <a className='nav-link ' href='/'>
                   Articles
                 </a>
               </li>
               <li className='nav-item'>
-                <a className='nav-link' href='#'>
+                <a className='nav-link' href='/'>
                   Pricing
                 </a>
               </li>
               <li className='nav-item'>
-                <a className='nav-link' href='#'>
+                <a className='nav-link' href='/'>
                   Why us?
                 </a>
               </li>
               <li className='nav-item'>
-                <a className='nav-link' href='#'>
+                <a className='nav-link' href='/'>
                   Join
                 </a>
               </li>
               <li className='nav-item'>
-                <a className='nav-link' href='#'>
+                <a className='nav-link' href='/'>
                   Terms
                 </a>
               </li>
