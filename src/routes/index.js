783d7c8d4fb24d5c1853deb5fd46c89206be9342
Neil101 <bagabagon111000@example.com>
2022-05-04 22:18:50 +0800

    added payment.js/.css & insert route in index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { AuthProvider, Protected } from '../helpers/Protected';
+import { AuthProvider, Protected } from "../helpers/Protected";
 // pages
-import Home from '../pages/Home';
-import Default from '../pages/Default';
-import NotFound from '../pages/NotFound';
-import About from '../pages/About';
-import MainLayout from '../pages/MainLayout';
-import SignUp from '../pages/SignUp';
+import Home from "../pages/Home";
+import Default from "../pages/Default";
+import NotFound from "../pages/NotFound";
+import About from "../pages/About";
+import MainLayout from "../pages/MainLayout";
+import SignUp from "../pages/SignUp";
 // import SignIn from '../pages/SignIn';
-import DashboardLayout from '../pages/DashboardLayout';
-import Project from '../pages/Project';
-import Login from '../pages/Login';
-import Unauthorized from '../pages/Unauthorized';
-import PersistLogin from '../pages/PersistLogin';
+import DashboardLayout from "../pages/DashboardLayout";
+import Project from "../pages/Project";
+import Login from "../pages/Login";
+import Unauthorized from "../pages/Unauthorized";
+import PersistLogin from "../pages/PersistLogin";
+import Payment from "../pages/Payment";
 
 const Router = () => {
   return (
@@ -22,32 +23,34 @@ const Router = () => {
       <AuthProvider>
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={<MainLayout />}>
+            <Route path="/" element={<MainLayout />}>
               <Route index element={<Home />} />
-              
+
               <Route element={<PersistLogin />}>
                 <Route element={<Protected roles={[2]} />}>
-                  <Route path='defaults' element={<Default />} />
+                  <Route path="defaults" element={<Default />} />
                 </Route>
               </Route>
 
-              <Route path='unauthorized' element={<Unauthorized />} />
+              <Route path="unauthorized" element={<Unauthorized />} />
 
-              <Route path='about' element={<About />} />
+              <Route path="about" element={<About />} />
             </Route>
 
-            <Route path='signup' element={<SignUp />} />
-            <Route path='signin' element={<Login />} />
+            <Route path="signup" element={<SignUp />} />
+            <Route path="signin" element={<Login />} />
 
             <Route element={<PersistLogin />}>
               <Route element={<Protected />}>
-                <Route path='dashboard' element={<DashboardLayout />}>
+                <Route path="dashboard" element={<DashboardLayout />}>
                   <Route index element={<Project />} />
                 </Route>
               </Route>
             </Route>
 
-            <Route path='*' element={<NotFound />} />
+            <Route path="payment" element={<Payment />} />
+
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
