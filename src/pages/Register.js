fce62b3b97ee7a318affe10e313cc3679f71a2ee
Marc Steven Esteban <marcesteban48@gmail.com>
2022-05-12 22:24:32 +0800

    Added Project Name validation

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import style from '../styles/Register.module.css';
 
 function Register() {
+  const initialValues = { projectname: ""};
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
   const handleSubmit = (e)=>{
     e.preventDefault()
-  }
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.projectname) {
+      errors.projectname = "Required field!";
+    }
+
+    return errors;
+  };
+
+  
 
   return (
-    <div className="SignUpcontainer">
+    <div className="Registercontainer">
       <nav className={style.nav_bar}>
         <h1>Project Registration Form</h1>
         <img src={"/assets/images/logo.png"} className={style.app_logo} alt="logo" />
@@ -19,20 +48,23 @@ function Register() {
        
         <h1>Registration Form</h1>
         <div className="field">
-            <label className="tittle1"> 1. Project Name</label>
+            <label className={style.tittle1}> 1. Project Name</label>
             <p className="subhead1">What is your project name or tittle?</p>
             <input
               type="text"
               name="projectname"
               placeholder="Project name"
+              value={formValues.projectname}
+              onChange={handleChange}
 
             />
           </div>
+          <p className ={style.text}>{formErrors.projectname}</p>
 
           <div className="field">
             
-          <label className="tittle1">2. Project Type </label>
-            <p className="subhead1"> Select your project type, or use other for different type</p>
+          <label className={style.tittle1}>2. Project Type </label>
+            <p className={style.subhead1}> Select your project type, or use other for different type</p>
             <select>
                 <option value="project type" selected disabled>
                 </option>
@@ -47,8 +79,8 @@ function Register() {
           
           
           <div className="field">
-            <label className="tittle1"> 3. Platform</label>
-            <p className="subhead1"> Choose your platform. (This will determine the price of your project)</p>
+            <label className={style.tittle1}> 3. Platform</label>
+            <p className={style.subhead1}> Choose your platform. (This will determine the price of your project)</p>
             <select>
                 <option value="project type" selected disabled>
                 </option>
@@ -59,7 +91,7 @@ function Register() {
           </div>
 
           
-          <button className="sbutton" type="submit">Submit</button>
+          <button className={style.sbutton} type="submit">Submit</button>
         </div>
         </div> 
       </form>
