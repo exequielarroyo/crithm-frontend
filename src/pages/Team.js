2416833178765799c51a0c5acd755a12299cbc14
Mher Jazer Albalate <albalate.jazer@gmail.com>
2022-05-17 11:05:48 +0800

    team

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -4,9 +4,28 @@ import style from "../styles/Team.module.css";
 const About = () => {
   return (
     <>
+      <div className={style.nav_team}>
+        <nav className={style.nav_bar}>
+          <h1 className={style.nav_title}>OUR TEAM</h1>
+          <p className={style.nav_content}>
+            We are a team of passionate and creative people who love to create
+            and build products.
+          </p>
+        </nav>
+      </div>
+
       <div className={style.about}>
         <div className={style.container}>
-          <div>hello</div>
+          <div>
+            <img src={"/assets/images/11.jpg"} alt="jazer.jpg" />
+
+            <h3>Mher Jazer P. Albalate</h3>
+          </div>
+
+          <p>
+            Software Developer of Crithm. A committed and humble person and a
+            person who looks for adventure and more challenging things.
+          </p>
         </div>
       </div>
     </>
