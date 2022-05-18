ce83e6d0d76c382f8593c600563fbd2a4864ff8d
Neil101 <bagabagon111000@example.com>
2022-05-18 17:16:22 +0800

    Added Reciept

--- src/pages/dashboard/payment/Receipt.js
+++ src/pages/dashboard/payment/Receipt.js
@@ -1,7 +1,45 @@
 import React from "react";
+import style from "../../../styles/Payment.module.css";
 
 const Receipt = () => {
-  return <div>Receipt</div>;
+  return (
+    <div className={style.billing_container}>
+      <div className={style.Billing}>
+        <form>
+          <div className={style.border}>
+            <div className={style.upper}>
+              <img src="/assets/images/logo.png" alt="logo" height="25" />
+              <h5>CRITHM</h5>
+              <div>
+                <h6>crithm.cf</h6>
+                <img className={style.side_design} src="/assets/images/receipt_side.png" />
+              </div>
+            </div>
+            <div className={style.title}>
+              <h3>Receipt of Payment</h3>
+              <line />
+              <div>
+                <label>
+                  Contact us: <a href="mailto:crithm.cf@gmail.com">crithm.cf@gmail.com</a>
+                </label>
+              </div>
+            </div>
+            <div className={style.body}>
+              <label className={style.plan}>Plan</label>
+              <label className={style.plan}>₱ 00.00</label>
+              <label>Sub Total</label>
+              <label>₱ 00.00</label>
+              <label>VAT %</label>
+              <label>10%</label>
+              <label className={style.total}>Total:</label>
+              <label className={style.total}>₱ 00.00</label>
+            </div>
+            <label className={style.lower}>Take a screen shot for proof of payment</label>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Receipt;
