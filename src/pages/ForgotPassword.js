3b770cf2d28cb3be16b8ca37a50bf5ec6dcd2e5b
Marc Steven Esteban <marcesteban48@gmail.com>
2022-05-20 19:56:15 +0800

    added ForgotPassword ui

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useForm } from "react-hook-form";
 import { useNavigate, useParams } from 'react-router-dom';
 import useAxiosPrivate from '../hooks/useAxiosPrivate';
+import style from '../styles/ForgotPass.module.css';
 
 const ForgotPassword = () => {
   const { register, handleSubmit } = useForm();
@@ -19,13 +20,35 @@ const ForgotPassword = () => {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(handlePassword)}>
-        <input placeholder="Password" name="password" {...register("password")} />
-        <button type="submit">Change password</button>
+    <div className="ForgotPasscontainer"> 
+      <div className={style.logoContainer}>
+      <div className={style.joinContainer}>
+          <div className={style.content}>
+            <h1>CRITHM</h1>
+            <div className={style.img}>
+              <img src="/assets/images/ring.png" alt="" />
+              <img src="/assets/images/curve-line.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <form  onSubmit= {handleSubmit}>
+        <div class ="card" className={style.uiform}>  
+        <div>
+        <h1>Change Password</h1>
+        <h2 className={style.subHead}>Choose a strong password and don't reuse it for other accounts!</h2>
+        <div className="field">
+          </div>
+          <form onSubmit={handleSubmit(handlePassword)}>
+              <input placeholder="Password" name="password" {...register("password")} />
+              <button type="submit">Change password</button>
+          </form>
+        </div>
+        </div> 
       </form>
     </div>
   );
 }
 
-export default ForgotPassword
+export default ForgotPassword;
