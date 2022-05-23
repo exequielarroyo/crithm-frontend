adfaf8849ab8d2c266dbcbad3822fe57818d4688
Exequiel Arroyo <zekielarroyo@gmail.com>
2022-05-23 14:43:13 +0800

    update loading animation

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -1,12 +1,13 @@
 import React from "react";
 import style from "../styles/Loading.module.css";
-import Lottie from "lottie-react";
-import groovyWalkAnimation from "../lottie/loading.json";
+// import Lottie from "lottie-react";
+// import groovyWalkAnimation from "../lottie/loading.json";
 
 const Loading = () => {
   return (
     <div className={style.container}>
-      <Lottie animationData={groovyWalkAnimation} style={{height: '200px'}}/>
+    <lottie-player src="/assets/lottie/loading.json"  background="transparent"  style={{height: '200px'}} loop  autoplay/>
+      {/* <Lottie animationData={groovyWalkAnimation} style={{height: '200px'}}/> */}
     </div>
   );
 };
