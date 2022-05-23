2009f4a48b26431fd251355cf209a326d6e7b356
Exequiel Arroyo <zekielarroyo@gmail.com>
2022-05-24 07:06:16 +0800

    update notfound page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import style from "../styles/Notfound.module.css";
+import { Player } from "@lottiefiles/react-lottie-player";
 
 const NotFound = () => {
   return (
-    <div className='content'>NotFound</div>
-  )
-}
+    <div className={style.container}>
+      <Player src="/assets/lottie/error.json" style={{ height: "200px" }} loop autoplay />
+      {/* <Lottie animationData={groovyWalkAnimation} style={{height: '200px'}}/> */}
+      <p>Page not found</p>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
