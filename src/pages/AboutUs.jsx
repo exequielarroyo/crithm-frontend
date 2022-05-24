ac5fb6d13939481c6200dfaaff2654af52ae8d8c
Exequiel Arroyo <zekielarroyo@gmail.com>
2022-05-24 15:24:16 +0800

    update about use

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -50,6 +50,75 @@ export default function AboutUs() {
           dependable, innovtive, and user-friendly software service provider.
         </JoinCard>
         <button className={style.buttonReg} onClick={()=>{navigate('/signup')}}>Register</button>
+
+        <div>
+        <div className={style.nav_team}>
+        <nav className={style.nav_bar}>
+          <h1 className={style.nav_title}>Meet our Dev Team</h1>
+          <p className={style.nav_content}>
+            Spanning far and wide, this small but mighty team is bringing developers new solutions to old problems. We're here to muddy the waters.
+          </p>
+        </nav>
+      </div>
+
+      <div className={style.about}>
+        <div className={style.profilecontainer}>
+          <div>
+            <div className={style.image1} />
+            <h3 className={`${style.padding} ${style.h3}`}>Albalate, Mher Jazer</h3>
+          </div>
+          <p className={`${style.padding} ${style.p}`}>
+            Software Developer of Crithm. A committed and humble person and a person who looks for adventure and more challenging things.
+          </p>
+        </div>
+
+        <div className={style.profilecontainer}>
+          <div>
+            <div className={style.image2} />
+            <h3 className={`${style.padding} ${style.h3}`}>Arroyo, Exequiel</h3>
+          </div>
+          <p className={`${style.padding} ${style.p}`}>
+            Software Developer of Crithm. A committed and humble person and a person who looks for adventure and more challenging things.
+          </p>
+        </div>
+        <div className={style.profilecontainer}>
+          <div>
+            <div className={style.image3} />
+            <h3 className={`${style.padding} ${style.h3}`}>Bagabagon, Neil Jordan</h3>
+          </div>
+          <p className={`${style.padding} ${style.p}`}>
+            Software Developer of Crithm. A committed and humble person and a person who looks for adventure and more challenging things.
+          </p>
+        </div>
+        <div className={style.profilecontainer}>
+          <div>
+            <div className={style.image4} />
+            <h3 className={`${style.padding} ${style.h3}`}>Bagalso, Robert</h3>
+          </div>
+          <p className={`${style.padding} ${style.p}`}>
+            Software Developer of Crithm. A committed and humble person and a person who looks for adventure and more challenging things.
+          </p>
+        </div>
+        <div className={style.profilecontainer}>
+          <div>
+            <div className={style.image5} />
+            <h3 className={`${style.padding} ${style.h3}`}>Esteban, Marc Steven</h3>
+          </div>
+          <p className={`${style.padding} ${style.p}`}>
+            Software Developer of Crithm. A committed and humble person and a person who looks for adventure and more challenging things.
+          </p>
+        </div>
+        <div className={style.profilecontainer}>
+          <div>
+            <div className={style.image6} />
+            <h3 className={`${style.padding} ${style.h3}`}>Gonzales, Marvin</h3>
+          </div>
+          <p className={`${style.padding} ${style.p}`}>
+            Software Developer of Crithm. A committed and humble person and a person who looks for adventure and more challenging things.
+          </p>
+        </div>
+      </div>
+        </div>
       </section>
     </>
   );
