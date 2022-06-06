4d6774f474ab4fce174073fe2660b08814501a86
Exequiel Arroyo <zekielarroyo@gmail.com>
2022-06-06 21:14:31 +0800

    changes in defaults

--- src/pages/Defaults.jsx
+++ src/pages/Defaults.jsx
@@ -1,15 +1,15 @@
-import { Scrollbar } from "smooth-scrollbar-react";
+// import { Scrollbar } from "smooth-scrollbar-react";
 // import Scrollbar from 'smooth-scrollbar';
 
 export default function Defaults() {
   return (
     <>
       <div style={{ height: "200px" }}>
-        <Scrollbar>
+        {/* <Scrollbar> */}
           <section style={{ height: "500px" }}>1</section>
           <section style={{ height: "500px" }}>1</section>
           <section style={{ height: "500px" }}>1</section>
-        </Scrollbar>
+        {/* </Scrollbar> */}
       </div>
     </>
   );
